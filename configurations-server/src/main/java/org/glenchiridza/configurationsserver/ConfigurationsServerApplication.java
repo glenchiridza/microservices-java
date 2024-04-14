a3a1d8405b999995f1b7ac81ea9115209270d78c
glenchiridza <glenchiridz@gmail.com>
2024-04-14 11:42:29 +0200

    configurations server

---
+++ configurations-server/src/main/java/org/glenchiridza/configurationsserver/ConfigurationsServerApplication.java
@@ -0,0 +1,13 @@
+package org.glenchiridza.configurationsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationsServerApplication.class, args);
+    }
+
+}
---
+++ configurations-server/src/test/java/org/glenchiridza/configurationsserver/ConfigurationsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.glenchiridza.configurationsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
